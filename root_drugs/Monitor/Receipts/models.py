8a87cf6d72556c07a73decfed2cc99d14266f23a
Kingyaga <marcellusobiajulu@gmail.com>
2023-12-07 19:38:11 -0800

    Added product model

--- root_drugs/Monitor/Receipts/models.py
+++ root_drugs/Monitor/Receipts/models.py
@@ -25,6 +25,13 @@ class Drug(models.Model):
 
     def __str__(self):
         return self.drug_name
+    
+class Provision(models.Model):
+    product_name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.product_name
 
 class SupplierSupply(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
